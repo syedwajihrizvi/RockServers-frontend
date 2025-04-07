cfad74f65d56dc4ee470176715673f0ad1aa8d3b
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2025-04-06 20:33:34 -0400

    Added new photos

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,38 +1,63 @@
-import WallPaperOne from "../assets/images/rockstart-games-wp.png"
-import WallPaperTwo from "../assets/images/rockstart-games-wp-2.png"
-import WallPaperThree from "../assets/images/rockstart-games-wp-3.png"
-import WallPaperFour from "../assets/images/rockstart-games-wp-4.png"
-import WallPaperFive from "../assets/images/rockstart-games-wp-5.png"
-import WallPaperSix from "../assets/images/rockstart-games-wp-6.png"
-import WallPaperSeven from "../assets/images/rockstart-games-wp-7.png"
-import { ReactNode, useEffect, useState } from "react"
+import { gsap } from "gsap"
+import { useGSAP } from "@gsap/react"
+import JaosonLucia from "../assets/images/rockstart-games-wp.webp"
+import FranklinChop from "../assets/images/rockstart-games-wp-2.webp"
+import JohnMarston from "../assets/images/rockstart-games-wp-3.webp"
+import ArthurMorgan from "../assets/images/rockstart-games-wp-4.webp"
+import Trevor from "../assets/images/rockstart-games-wp-5.webp"
+import Sadie from "../assets/images/rockstart-games-wp-6.webp"
+import Michael from "../assets/images/rockstart-games-wp-7.webp"
+import { ReactNode, useEffect, useState, useRef } from "react"
+
+interface WallpaperProps {
+  location: string,
+  class: string,
+  alt: string
+}
 
 export const Account = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
+  const [prevImageIndex, setPrevImageIndex] = useState<number | null>(null)
 
-  const images : ReactNode[] = [
-    <img className="account-container__img account-container__img-one" src={WallPaperOne} alt="Niko, Max Payne, JM"/>,
-    <img className="account-container__img account-container__img-two" src={WallPaperTwo} alt="Franklin and Chop"/>,
-    <img className="account-container__img account-container__img-two" src={WallPaperThree} alt="John Marston"/>,
-    <img className="account-container__img account-container__img-two" src={WallPaperFour} alt="Arthur Morgan"/>,
-    <img className="account-container__img account-container__img-two" src={WallPaperFive} alt="Arthur Morgan"/>,
-    <img className="account-container__img account-container__img-two" src={WallPaperSix} alt="Arthur Morgan"/>,
-    <img className="account-container__img account-container__img-two" src={WallPaperSeven} alt="Arthur Morgan"/>
+  const images : WallpaperProps[] = [
+    {location: JaosonLucia, class: "account-container__img-one", alt: ""},
+    {location: FranklinChop, class: "account-container__img-two", alt: ""},
+    {location: JohnMarston, class: "account-container__img-two", alt: ""},
+    {location: ArthurMorgan, class: "account-container__img-two", alt: ""},
+    {location: Trevor, class: "account-container__img-two", alt: ""},
+    {location: Sadie, class: "account-container__img-two", alt: ""},
+    {location: Michael, class: "account-container__img-two", alt: ""},
   ]
 
+  const imageRefs = useRef<(HTMLImageElement | null)[]>([...new Array(images.length).fill(null)])
+
+  const imageNodes : ReactNode[] = images.map((image, idx) => 
+    <img key={idx} ref={(element) => {imageRefs.current[idx] = element}} 
+         className={`account-container__img ${image.class}`} 
+         src={image.location} alt={image.alt}/>
+  )
   useEffect(() => {
     const handleClick = () => {
-      setCurrentImageIndex(currentImageIndex == images.length - 1 ? 0 : currentImageIndex + 1)
+      const nextIndex = currentImageIndex == images.length - 1 ? 0 : currentImageIndex + 1
+      setPrevImageIndex(currentImageIndex)
+      setCurrentImageIndex(nextIndex)
     }
     window.addEventListener('click', handleClick)
 
     return () => window.removeEventListener('click', handleClick)
   })
 
+  useGSAP(() => {
+    const tl = gsap.timeline()
+    if (prevImageIndex != null)
+      tl.to(imageRefs.current[prevImageIndex], { opacity: 0, duration: 0.5})
+    tl.to(imageRefs.current[currentImageIndex], { opacity: 1, duration: 0.5})
+  }, [currentImageIndex])
+
   return (
     <div className="account-container">
       <div>
-        {images[currentImageIndex]}
+        {imageNodes}
       </div>
     </div>
   )
