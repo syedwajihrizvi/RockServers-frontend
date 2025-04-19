c920f23bde27c08dc8f90a6442a0296096f37e12
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2025-04-18 23:18:27 -0400

    Create basic post creation

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,22 +1,63 @@
-import { useState } from "react"
+import { FaUpload } from "react-icons/fa6"
+import { IoMdCloseCircle, IoMdCheckmarkCircle } from "react-icons/io";
+import { FaTrash } from "react-icons/fa"
+import { IoCloseCircle } from "react-icons/io5"
+import { useEffect, useState } from "react"
+import { useGames } from "../hooks/useGames";
+import { IImage } from "../utils/interfaces/Interfaces";
+import apiClient from "../utils/services/dataServices"
+import { generateImageUrl } from "../utils/helpers/helpers";
+
+interface PostData {
+  title?: string,
+  content?: string,
+  gameId?: number,
+  imageSelected?: string
+}
 
 export const Create = () => {
   const [creatingPost, setCreatingPost] = useState(true)
+  const [choosingImage, setChoosingImage] = useState(false)
+  const [postData, setPostData] = useState<PostData>({})
+  const [gameImages, setGameImages] = useState<IImage[]>([])
+  const { data: games, isLoading: isLoadingGames } = useGames()
+  
+
+  useEffect(() => {
+    if (postData.gameId) {
+      apiClient.get(`/images?gameId=${postData.gameId}`)
+      .then(res => setGameImages([...res.data]))
+      .catch(() => console.log("An Error occured"))
+    }
+  }, [postData.gameId])
 
   return (
-    <div className="create-container">
+    <div className="create-container--wrapper">
+      {choosingImage && <div className="create-choose-image">
+        <IoMdCloseCircle className="close-icon" onClick={() => setChoosingImage(false)}/>
+        <h1>Showing images for Game.</h1>
+        <div className="create-choose-image__img">
+          {gameImages.map(gameImage => 
+            <div className="create-img__container">
+              {postData && postData.imageSelected && 
+              postData.imageSelected == gameImage.imagePath && <IoMdCheckmarkCircle className="icon"/>}
+              <img onClick={() => setPostData({...postData, imageSelected: !postData ? gameImage.imagePath : postData.imageSelected == gameImage.imagePath ? undefined :  gameImage.imagePath})} 
+                   src={generateImageUrl(gameImage.imagePath)}/>
+            </div>)}
+        </div>
+      </div>}
+      <div className="create-container" style={{opacity: choosingImage ? 0.6 : 1}}>
           <div className="create-type">
               <button className="btn btn--success btn--md" onClick={() => setCreatingPost(true)}>Post a session</button>
               <button className="btn btn--success btn--md" onClick={() => setCreatingPost(false)}>Discuss Something</button>
           </div>
-          {creatingPost && <div className="create-post">
+          {creatingPost && !isLoadingGames && games && <div className="create-post">
             <div className="create-type">
-              <select className="create-option">
+              <select className="create-option" 
+                      onChange={(event) => setPostData({...postData, gameId: parseInt(event.target.value)})}>
                 <option>Choose a Game</option>
-                <option>RDR2</option>
-                <option>RDR1</option>
-                <option>GTA 5</option>
-                <option>GTA 6</option>
+                {games.map(game => 
+                  <option key={game.id} value={game.id}>{game.title}</option>)}
               </select>
               <select className="create-option">
                 <option>What Platform</option>
@@ -27,10 +68,24 @@ export const Create = () => {
             </div>
             <input placeholder="Enter a Title" className="create-input"/>
             <textarea placeholder="Enter a description" className="create-textarea"></textarea>
+            <div className="create-upload-type">
+              <label className="file-upload-wrapper btn btn--success btn--md">
+                Upload Image <FaUpload className="icon"/>
+                <input type="file"/>
+              </label>
+              <button className="btn btn--success btn--sm" onClick={() => setChoosingImage(true)}>Choose Ready Image</button>
+            </div>
+            <div className="create-post__img--selected">
+              {postData && postData.imageSelected && 
+              <div className="create-post__img--selected__wrapper">
+                <img src={generateImageUrl(postData.imageSelected)} onClick={() => setChoosingImage(true)}/>
+              </div>}
+            </div>
           </div>}
           {!creatingPost && <div className="create-discussion">
             <h1>Create Discussion</h1>
           </div>}
+    </div>
     </div>
   )
 }
--- src/utils/interfaces/Interfaces.ts
+++ src/utils/interfaces/Interfaces.ts
@@ -74,3 +74,7 @@ export interface IUser {
     likedComments: number[],
     likedDiscussionComments: number[]
 }
+
+export interface IImage {
+    imagePath: string
+}
